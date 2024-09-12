b1db5da88a011f201f1744c83097d2717edf90fb
Elisson Rocha <elissonrochaa@gmail.com>
2024-09-11 22:23:44 -0300

    create eventhub and config database

---
+++ eventhub/src/main/java/br/org/upe/eventhub/EventhubApplication.java
@@ -0,0 +1,13 @@
+package br.org.upe.eventhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhubApplication.class, args);
+	}
+
+}
